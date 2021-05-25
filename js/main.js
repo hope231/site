413f7fbf9c652b7cdcaf210287f4bfbf4e18370d
hope231 <84771862+hope231@users.noreply.github.com>
2021-05-25 12:50:17 +0300

    site

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('.header-burger').click(function(event){
+		$('.header-burger,.header-body,.header-body__colum__logo').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
